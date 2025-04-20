3828763df81e8878ff3fecba502e35f55f38d396
habib610 <habib54562@gmail.com>
2025-04-20 13:32:09 +0600

    loosly-coupled-code

---
+++ src/main/java/org/rhabib/store/PaypalPaymentService.java
@@ -0,0 +1,9 @@
+package org.rhabib.store;
+
+public class PaypalPaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paypal ");
+        System.out.println("Amount: " + amount);
+    }
+}
